f3955cefc561b75bd0f289bdc5047f16ef9663a0
Pannio <1821299140@qq.com>
2023-08-18 18:13:34 +0800

    10.23

--- src/components/RepositoryList.jsx
+++ src/components/RepositoryList.jsx
@@ -2,6 +2,9 @@ import { FlatList, View, StyleSheet, Pressable } from 'react-native';
 import RepositoryItem from './RepositoryItem'
 import useRepositories from '../hooks/useRepositories';
 import { useNavigate } from 'react-router-native'
+import { useState, useEffect } from 'react'
+import { Picker } from '@react-native-picker/picker';
+
 
 const styles = StyleSheet.create({
   separator: {
@@ -9,11 +12,43 @@ const styles = StyleSheet.create({
   }
 });
 
+const orderMap = {
+  createdAtDesc: {
+    orderBy: 'CREATED_AT',
+  },
+  ratingDesc: {
+    orderBy: 'RATING_AVERAGE',
+    orderDirection: 'DESC'
+  },
+  ratingAsc: {
+    orderBy: 'RATING_AVERAGE',
+    orderDirection: 'ASC'
+  }
+}
 
+const RepositoryListHeader = ({ refetch }) => {
+  const [orderBy, setOrderBy] = useState('createdAtDesc')
+  useEffect(() => {
+    refetch(orderMap[orderBy])
+  }, [orderBy])
+  return (
+    <Picker
+      selectedValue={orderBy}
+      onValueChange={(itemValue) =>
+        setOrderBy(itemValue)
+      }
+      prompt='Select an item'
+    >
+      <Picker.Item label="Latest repositories" value="createdAtDesc" />
+      <Picker.Item label="Highest rated repositories" value="ratingDesc" />
+      <Picker.Item label="Lowest rated repositories" value="ratingAsc" />
+    </Picker>
+  )
+}
 
 const ItemSeparator = () => <View style={styles.separator} />
 
-export const RepositoryListContainer = ({ repositories }) => {
+export const RepositoryListContainer = ({ repositories, refetch }) => {
   const repositoryNodes = repositories ? repositories.edges.map(edge => edge.node) : []
   const navigate = useNavigate()
   const toViewRepository = (id) => {
@@ -23,6 +58,7 @@ export const RepositoryListContainer = ({ repositories }) => {
     <FlatList
       data={repositoryNodes}
       ItemSeparatorComponent={ItemSeparator}
+      ListHeaderComponent={<RepositoryListHeader refetch={refetch} />}
       renderItem={({ item }) => {
         return (
           <Pressable style={styles.item} onPress={() => toViewRepository(item.id)}>
@@ -35,8 +71,8 @@ export const RepositoryListContainer = ({ repositories }) => {
 }
 
 const RepositoryList = () => {
-  const { repositories } = useRepositories()
-  return <RepositoryListContainer repositories={repositories} />
+  const { repositories, refetch } = useRepositories()
+  return <RepositoryListContainer repositories={repositories} refetch={refetch} />
 }
 
 export default RepositoryList
--- src/graphql/queries.js
+++ src/graphql/queries.js
@@ -3,8 +3,8 @@ import { CORE_REPOSITORY_FIELDS } from './fragments'
 
 export const GET_REPOSITORIES = gql`
   ${CORE_REPOSITORY_FIELDS}
-  query Repositories {
-    repositories {
+  query Repositories($orderDirection: OrderDirection, $orderBy: AllRepositoriesOrderBy){
+    repositories(orderDirection: $orderDirection, orderBy: $orderBy) {
       edges {
         node {
           ...CoreRepositoryFields
